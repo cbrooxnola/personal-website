66c85d8d5f7dafbfd353d53b92a87b7afd012b5c
Christopher <csbroox@gmail.com>
2023-08-09 18:04:58 -0500

    pretty much done

--- javascript.js
+++ javascript.js
@@ -1,1 +1,15 @@
 console.log("Hello World!");
+//chessboard for practice (from Eloquent Javascript)
+let output = '';
+for (let i = 0; i <= 7; i++){
+  for (let n = 0; n <= 7; n++){
+    if ((i + n) % 2 === 0) {
+      output += '#';
+    }
+    else {
+      output += ' ';
+    }
+  }
+  output += '\n';
+}
+console.log(output);
